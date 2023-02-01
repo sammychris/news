bbd4031991402c840af67187bf3d2ad1aa696b75
SammyChris <ebusameric@gmail.com>
2023-02-01 23:19:58 +0100

    feature: add layout component to __app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Layout from "../lib/Layout";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Layout state={false}>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
