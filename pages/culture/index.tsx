4bc45d5fe1a4165f336746a5a0a88f794d68336a
SammyChris <ebusameric@gmail.com>
2023-02-11 09:22:28 +0100

    refactor: change page styling and functionality

--- pages/culture/index.tsx
+++ pages/culture/index.tsx
@@ -1,8 +1,13 @@
-import styles from "@/styles/Home.module.css";
+import Header from "../../components/Header";
 import HorizontalCard from "../../components/HorizontalCard";
 import { GetServerSideProps } from "next";
-import { imPoweredRequest } from "../../lib/request";
+import { imPoweredRequest } from "../../utils/api-request";
+import styles from "../../styles/Home.module.css";
 import Loader from "@/components/Loader";
+import Ads from "@/components/Ads";
+import SignUpForm from "@/components/SignUpForm";
+import NewsHub from "@/components/NewsHub";
+import ViewMoreButton from "@/components/ViewMoreButton";
 
 type SectionProps = {
   type: string;
@@ -33,11 +38,27 @@ interface Props {
 const Culture: React.FC<Props> = (props) => {
   const { size, posts } = props;
   if (!props?.posts.length) return <Loader />;
+  const posts1 = posts.slice(0, 3);
+  const posts2 = posts.slice(3, 6);
   return (
     <>
-      <div className={styles.container}>
-        <div>
-          {posts?.map((post: PostProps) => {
+      <div className={styles.collectionHeaderBG}>
+        <header className={styles.collectionHeader}>
+          <h1>Culture</h1>
+          <p>
+            Stay ahead of the game with Verge Culture, your go-to source for
+            daily political updates. We bring you the latest in government and
+            international relations, keeping a close eye on how current events
+            shape the political landscape. Our articles are carefully crafted to
+            provide you with the most comprehensive and informed political
+            coverage.
+          </p>
+        </header>
+      </div>
+
+      <div className={styles.collectionContainer}>
+        <div className={styles.leftSide}>
+          {posts1?.map((post: PostProps) => {
             let img_header = "";
             let vid_header = "";
             post?.sections?.forEach((section: SectionProps) => {
@@ -65,6 +86,51 @@ const Culture: React.FC<Props> = (props) => {
             );
           })}
         </div>
+        <div className={styles.rightSide}>
+          <Ads posts={posts1} />
+        </div>
+      </div>
+
+      <div className={styles.collectionContainer}>
+        <div>
+          {posts1?.map((post: PostProps) => {
+            let img_header = "";
+            let vid_header = "";
+            post?.sections?.forEach((section: SectionProps) => {
+              if (section.type === "IMAGE" && img_header === "") {
+                img_header = section.image;
+              }
+
+              if (section.type === "VIDEO" && img_header === "") {
+                vid_header = section.video;
+              }
+            });
+
+            const title = post?.title?.trim() || post.sub_title;
+            return (
+              <HorizontalCard
+                link={`/culture/${post.id}/${title
+                  ?.replace(/\s/g, "-")
+                  .toLowerCase()}`}
+                title={title}
+                image={img_header}
+                text={post.sections[0].text}
+                video={vid_header}
+                key={post.id}
+              />
+            );
+          })}
+        </div>
+        <div>
+          <SignUpForm />
+        </div>
+      </div>
+      <div className={styles.container}>
+        <NewsHub title="Related News" posts={posts} />
+      </div>
+
+      <div className={styles.container}>
+        <ViewMoreButton title="View More in Related News" />
       </div>
     </>
   );
